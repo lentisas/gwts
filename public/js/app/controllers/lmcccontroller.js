0f34deb44d3c2184783190d6d35e0cbc0dec5d78
adjetey <adjeteyadjei@live.com>
2013-04-09 23:00:17 +0000

    added save

--- public/js/app/controllers/lmcccontroller.js
+++ public/js/app/controllers/lmcccontroller.js
@@ -21,35 +21,38 @@ function LMCCController ($scope, LMCC, $http) {
 		//console.log("gets here");
 	}
 
-	$scope.back = function () {
+	$scope.back = function (log) {
+		//console.log(log);
+		var theLog = angular.copy(log);
+		theLog.id = $scope.lmccLogs.length;
+		$scope.lmccLogs.push(theLog);
 		$('#lmcc_tabs a:first').tab('show');
 	}
 
-	$scope.addLog = function (log) {
-		var log = {
-			id : $scope.lmccLogs.length + 1,
-			tifRef : log.tifRef,
-			reserveCode: log.reserveCode,
-			compartmentNumber: log.compartmentNumber,
-			stockNumber: log.stockNumber,
-			treeNumber : log.treeNumber,
-			logNumber : log.logNumber,
-			species: log.species,
-			db1: log.db1,
-			db2: log.db2,
-			db3: log.db3,
-			dt1: log.dt1,
-			dt2: log.dt2,
-			dt3: log.dt3,
-			length: log.length,
-			vol: log.vol,
-			defects: log.defects,
-			grade:log.grade
-		}
-
-
-		$scope.lmccLogs.push(log);
+	$scope.saveLMCC = function () {
+		var lmcc = new LMCC($scope.newLMCC);
+		lmcc.logs = $scope.lmccLogs;
+		lmcc.$save(function (res) {
+			afterSave(res);
+		})
 	}
+
+	function afterSave(res,callback){
+        var msg = "";
+        if(res.success){
+            msg = res.message;
+            MSG.show(msg,"success");
+            $scope.newLMCC = {};
+            $scope.lmccLogs = [];
+            $scope.newLog = {};
+            //any other business
+            if(callback)
+                callback();
+        }else {
+            msg = res.message || "Sorry, errors were ecountered";
+            MSG.show(msg);
+        }
+    }
 
 	getContractors();
 	getForestDistricts();
